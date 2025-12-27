360a22dd2db78dae74e81bb355dfa2a31a69f4c8
Bhavyasri Chippala <bhavyyasrichippala.com>
2025-12-27 16:44:28 +0530

    Secure Firebase configuration using environment variables

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,17 +1,19 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 import { getStorage } from "firebase/storage";
+
 const firebaseConfig = {
-  // ⚠️ PASTE YOUR REAL KEYS HERE
-  apiKey: "AIzaSyAl-w0LJKmxv0OTqfkDdERyM3C1BvRTvjc",
-  authDomain: "my-lms-d60d3.firebaseapp.com",
-  databaseURL: "https://my-lms-d60d3-default-rtdb.firebaseio.com/",
-  projectId: "my-lms-d60d3",
-  storageBucket: "my-lms-d60d3.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcdef"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
+
 export const db = getDatabase(app);
 export const storage = getStorage(app);
+export default app;
